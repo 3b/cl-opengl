5a4ebfbdd28463431d714f004605e11d781ce23b
Boian Tzonev <boiantz@gmail.com>
2011-01-31 23:03:51 +0200

    Set colors of the star

--- examples/redbook/tess.lisp
+++ examples/redbook/tess.lisp
@@ -19,7 +19,6 @@
 
 (in-package #:cl-glut-examples)
 
-;;;GLuint startList;
 (defparameter *start-list* nil)
 
 (defclass tess-window (glut:window)
@@ -28,6 +27,9 @@
                      :mode '(:single :rgb)))
 
 (defclass example-tessellator (glu:tessellator)
+  ())
+
+(defclass star-tessellator (glu:tessellator)
   ())
 
 (defmethod glut:display ((w tess-window))
@@ -50,37 +52,49 @@
 
 (defmethod glu:tess-vertex-data-cb ((tess example-tessellator) vertex-data polygon-data)
   (let ((varray (gl::make-gl-array-from-pointer vertex-data '%gl:double 3)))
-    (format t "vertex ~a,~a,~a~%" 
-            (gl:glaref varray 0)
-            (gl:glaref varray 1)
-            (gl:glaref varray 2))
     (gl:vertex (gl:glaref varray 0)
-               (gl:glaref varray 1))))
+               (gl:glaref varray 1)
+               (gl:glaref varray 3))))
 
-(defmethod glu:tess-combine-data-cb ((tess example-tessellator) coords vertex-data weight data-out polygon-adata)
-  (let ((vertex (cffi:foreign-alloc '%gl:double :count 3)))
+(defmethod glu:tess-vertex-data-cb ((tess star-tessellator) vertex-data polygon-data)
+  (let ((varray (gl::make-gl-array-from-pointer vertex-data '%gl:double 6)))
+        (gl:color (gl:glaref varray 3) (gl:glaref varray 4) (gl:glaref varray 5))
+        (gl:vertex (gl:glaref varray 0) (gl:glaref varray 1) (gl:glaref varray 2))))
+
+(defmethod glu:tess-combine-data-cb ((tess star-tessellator) coords vertex-data weight data-out polygon-adata)
+  (let ((vertex (cffi:foreign-alloc '%gl:double :count 6)))
     (loop for i from 0 below 3
        do (setf (cffi:mem-aref vertex '%gl:double i)
                 (cffi:mem-aref coords '%gl:double i)))
     
     ;;TODO set colours
-                   
+    (loop for i from 3 below 6
+         do (setf (cffi:mem-aref vertex '%gl:double i)
+                  (+ 
+                   (* (cffi:mem-aref weight '%gl:float 0)
+                      (cffi:mem-aref (cffi:mem-aref vertex-data :pointer 0) '%gl:double i))
+                   (* (cffi:mem-aref weight '%gl:float 1)
+                      (cffi:mem-aref (cffi:mem-aref vertex-data :pointer 1) '%gl:double i))
+                   (* (cffi:mem-aref weight '%gl:float 2)
+                      (cffi:mem-aref (cffi:mem-aref vertex-data :pointer 2) '%gl:double i))
+                   (* (cffi:mem-aref weight '%gl:float 3)
+                      (cffi:mem-aref (cffi:mem-aref vertex-data :pointer 3) '%gl:double i)))))
     (setf (cffi:mem-ref data-out :pointer) vertex)))
 
 (defun init ()
   (let ((tobj (make-instance 'example-tessellator))
-        (rect '((50.0 50.0 0.0)
-                (200.0 50.0 0.0)
-                (200.0 200.0 0.0)
-                (50.0 200.0 0.0)))
-        (tri '((75.0 75.0 0.0)
-               (125.0 175.0 0.0)
-               (175.0 75.0 0.0)))
-        (star '((250.0 50.0 0.0 1.0 0.0 1.0)
-                  (325.0 200.0 0.0 1.0 1.0 0.0)
-                  (400.0 50.0 0.0 0.0 1.0 1.0)
-                  (250.0 150.0 0.0 1.0 0.0 0.0)
-                  (400.0 150.0 0.0 0.0 1.0 0.0))))
+        (rect '((50 50 0)
+                (200 50 0)
+                (200 200 0)
+                (50 200 0)))
+        (tri '((75 75 0)
+               (125 175 0)
+               (175 75 0)))
+        (star '((250 50 0 1 0 1)
+                  (325 200 0 1 1 0)
+                  (400 50 0 0 1 1)
+                  (250 150 0 1 0 0)
+                  (400 150 0 0 1 0))))
 
     (gl:clear-color 0 0 0 0)
     (setf *start-list* (gl:gen-lists 2))
@@ -100,18 +114,19 @@
     (glu:tess-end-contour tobj)
     (glu:tess-end-polygon tobj)
     (gl:end-list)
-
+    
   ;;;; smooth shaded, self-intersecting star
-  (gl:new-list (1+ *start-list*) :compile)
-  (gl:shade-model :smooth)
-  (glu:tess-property tobj :tess-winding-rule glu:+tess-winding-positive+)
-  (glu:tess-begin-polygon tobj nil)
-  (glu:tess-begin-contour tobj)
-  (loop for coords in star
+    (setf tobj (make-instance 'star-tessellator))
+    (gl:new-list (1+ *start-list*) :compile)
+    (gl:shade-model :smooth)
+    (glu:tess-property tobj :tess-winding-rule glu:+tess-winding-positive+)
+    (glu:tess-begin-polygon tobj nil)
+    (glu:tess-begin-contour tobj)
+    (loop for coords in star
        do (glu:tess-vertex tobj coords))                           
-  (glu:tess-end-contour tobj)
-  (glu:tess-end-polygon tobj)
-  (gl:end-list)))
+    (glu:tess-end-contour tobj)
+    (glu:tess-end-polygon tobj)
+    (gl:end-list)))
 
 (defun rb-tess ()
   (setf glut:*run-main-loop-after-display* nil)
