202dd14f475a2663e0bdd4d76a2e973095e342a6
Bart Botta <00003b@gmail.com>
2011-05-20 18:34:24 +0800

    add enums for more mouse buttons
    not sure how many are valid, but my mouse seems to return clicks for
    buttons it doesn't even have...

--- glut/callbacks.lisp
+++ glut/callbacks.lisp
@@ -189,7 +189,32 @@
   :button4
   :button5
   :button6
-  :button7)
+  :button7
+  :button8
+  :button9
+  :button10
+  :button11
+  :button12
+  :button13
+  :button14
+  :button15
+  :button16
+  :button17
+  :button18
+  :button19
+  :button20
+  :button21
+  :button22
+  :button23
+  :button24
+  :button25
+  :button26
+  :button27
+  :button28
+  :button29
+  :button30
+  :button31
+  :button32)
 
 (defcenum mouse-button-state
   (:wheel-down -1)
