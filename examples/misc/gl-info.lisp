115fbbe7d1f4494f652ad9f95c932c611e6d267b
Bart Botta <00003b@gmail.com>
2014-11-17 06:27:01 -0600

    add more values to gl-info example

--- examples/misc/gl-info.lisp
+++ examples/misc/gl-info.lisp
@@ -22,49 +22,77 @@
     (format t "GL renderer: ~a~%" (get* :renderer))
     (format t "GL extensions: ~a~%" (get* :extensions))
 
-    (loop for i in '(:max-clip-planes
+    (loop for i in '(:max-clip-planes ;
+                     ;; 23.64 implementation dependent values
                      :max-clip-distances
+                     :max-cull-distances
+                     :max-combined-clip-and-cull-distances
+                     :subpixel-bits
+                     :max-elements-indices ; :max-elements-index ?
+                     :primitive-restart-for-patches-supported
                      :max-3d-texture-size
                      :max-texture-size
                      :max-array-texture-layers
                      :max-texture-lod-bias
                      :max-cube-map-texture-size
                      :max-renderbuffer-size
+                     ;; 23.65 ; compat
+                     :max-lights
+                     :max-color-matrix-stack-depth ;; imaging subset
+                     :max-modelview-stack-depth
+                     :max-projection-stack-depth
+                     :max-texture-stack-depth
                      :max-pixel-map-table
                      :max-name-stack-depth
                      :max-list-nesting
                      :max-eval-order
+                     :max-attrib-stack-depth
+                     :max-client-attrib-stack-depth
+                     :aliased-point-size-range
+                     ;; :max-convolution-width ;; imaging subset
+                     ;; :max-convolution-height ;; imaging subset
+                     ;; 23.66
                      :max-viewport-dims
                      :max-viewports
                      :viewport-subpixel-bits
                      :viewport-bounds-range
-                     :max-attrib-stack-depth
-                     :max-client-attrib-stack-depth
-                     :aliased-point-size-range
+                     :layer-provoking-vertex
+                     :viewport-index-provoking-vertex
                      :point-size-range
                      :point-size-granularity
-                     :smooth-point-size-range
-                     :smooth-point-size-granularity
                      :aliased-line-width-range
-                     :line-width-range
-                     :line-width-granularity
                      :smooth-line-width-range
                      :smooth-line-width-granularity
                      :max-elements-indices
                      :max-elements-vertices
-                     :compressed-texture-formats
+                     ;; 23.67
+                     :max-vertex-attrib-relative-offset
+                     :max-vertex-attrib-bindings
+                     :max-vertex-attrib-stride
                      :num-compressed-texture-formats
+                     :compressed-texture-formats
+                     :max-texture-buffer-size
+                     :max-rectangle-texture-size
                      :num-program-binary-formats
                      :program-binary-formats
                      :num-shader-binary-formats
                      :shader-binary-formats
                      :shader-compiler
                      :min-map-buffer-alignment
-                     :max-texture-buffer-size
-                     :max-rectangle-texture-size
-                     :num-extensions
+                     :texture-buffer-offset-alignment
+                     ;; 23.68 implementation dependent version and
+                     ;; extension support
+                     :major-version
+                     :minor-version
                      :context-flags
-                     :max-texture-units
+                     ;; :extensions
+                     :num-extensions
+                     :renderer
+                     :shading-language-version
+                     :num-shading-language-versions
+                     :vendor
+                     :version
+                     ;; 23.69 implementation dependent vertex shader limits
                      :max-vertex-attribs
                      :max-vertex-uniform-components
                      :max-vertex-uniform-vectors
@@ -73,24 +101,33 @@
                      :max-vertex-texture-image-units
                      :max-vertex-atomic-counter-buffers
                      :max-vertex-atomic-counters
+                     :max-vertex-shader-storage-blocks
+                     ;; 23.70 implementation dependent tessellation
+                     ;; control shader limits
                      :max-tess-gen-level
                      :max-patch-vertices
                      :max-tess-control-uniform-components
-                     :max-tess-evaluation-uniform-components
                      :max-tess-control-texture-image-units
-                     :max-tess-evaluation-texture-image-units
                      :max-tess-control-output-components
                      :max-tess-patch-components
                      :max-tess-control-total-output-components
-                     :max-tess-evaluation-output-components
                      :max-tess-control-input-components
-                     :max-tess-evaluation-input-components
                      :max-tess-control-uniform-blocks
-                     :max-tess-evaluation-uniform-blocks
                      :max-tess-control-atomic-counter-buffers
-                     :max-tess-evaluation-atomic-counter-buffers
                      :max-tess-control-atomic-counters
+                     :max-tess-control-shader-storage-blocks
+                     ;; 23.71 implementation dependent tessellation
+                     ;; shader limits
+                     :max-tess-evaluation-uniform-components
+                     :max-tess-evaluation-texture-image-units
+                     :max-tess-evaluation-output-components
+                     :max-tess-evaluation-input-components
+                     :max-tess-evaluation-uniform-blocks
+                     :max-tess-evaluation-atomic-counter-buffers
                      :max-tess-evaluation-atomic-counters
+                     :max-tess-evaluation-shader-storage-blocks
+                     ;; 23.72 implementation dependent geometry shader
+                     ;; limits
                      :max-geometry-uniform-components
                      :max-geometry-uniform-blocks
                      :max-geometry-input-components
@@ -102,17 +139,37 @@
                      :max-vertex-streams
                      :max-geometry-atomic-counter-buffers
                      :max-geometry-atomic-counters
+                     :max-geometry-shader-storage-blocks
+                     ;; 23.73 implementation dependent fragment shader
+                     ;; limits
                      :max-fragment-uniform-components
                      :max-fragment-uniform-vectors
                      :max-fragment-uniform-blocks
                      :max-fragment-input-components
-                     :max-fragment-atomic-counter-buffers
-                     :max-fragment-atomic-counters
+                     :max-texture-image-units
                      :min-program-texture-gather-offset
                      :max-program-texture-gather-offset
-                     :max-texture-image-units
-                     :max-texture-units
-                     :max-texture-coords
+                     :max-texture-units ;; compat
+                     :max-texture-coords ;; compat
+                     :max-fragment-atomic-counter-buffers
+                     :max-fragment-atomic-counters
+                     :max-fragment-shader-storage-blocks
+                     ;; 23.74 implementation dependent compute shader
+                     ;; limits
+                     :max-compute-work-group-count
+                     :max-compute-work-group-size
+                     :max-compute-work-group-invocations
+                     :max-compute-uniform-blocks
+                     :max-compute-texture-image-units
+                     :max-compute-atomic-counter-buffers
+                     :max-compute-atomic-counters
+                     :max-compute-shared-memory-size
+                     :max-compute-uniform-components
+                     :max-compute-image-uniforms
+                     :max-combined-compute-uniform-components
+                     :max-compute-shader-storage-blocks
+                     ;; 23.75 implementation dependent aggregate
+                     ;; limits
                      :min-program-texel-offset
                      :max-program-texel-offset
                      :max-uniform-buffer-bindings
@@ -124,12 +181,19 @@
                      :max-combined-texture-image-units
                      :max-subroutines
                      :max-subroutine-uniform-locations
+                     :max-uniform-locations
+                     ;; 23.76
                      :max-atomic-counter-buffer-bindings
                      :max-atomic-counter-buffer-size
                      :max-combined-atomic-counter-buffers
                      :max-combined-atomic-counters
+                     :max-shader-storage-buffer-bindings
+                     :max-shader-storage-block-size
+                     :max-combined-shader-storage-blocks
+                     :shader-storage-buffer-offset-alignment
+                     ;; 23.77
                      :max-image-units
-                     :max-combined-image-units-and-fragment-outputs
+                     :max-combined-shader-output-resources
                      :max-image-samples
                      :max-vertex-image-uniforms
                      :max-tess-control-image-uniforms
@@ -137,31 +201,58 @@
                      :max-geometry-image-uniforms
                      :max-fragment-image-uniforms
                      :max-combined-image-uniforms
+                     ;; 23.78
                      :max-combined-vertex-uniform-components
                      :max-combined-geometry-uniform-components
                      :max-combined-tess-control-uniform-components
                      :max-combined-tess-evaluation-uniform-components
                      :max-combined-fragment-uniform-components
+                     ;; 23.80 implementation dependent debug output
+                     ;; state
+                     :max-debug-message-length
+                     :max-debug-logged-messages
+                     :max-debug-group-stack-depth
+                     :max-label-length
+                     ;; 23.81 implementation dependent values
+                     :max-framebuffer-width
+                     :max-framebuffer-height
+                     :max-framebuffer-layers
+                     :max-framebuffer-samples
                      :max-sample-mask-words
                      :max-samples
                      :max-color-texture-samples
                      :max-depth-texture-samples
                      :max-integer-samples
-                     :quads-follow-provoking-vertex-convention
+                     :quads-follow-provoking-vertex-convention ;; compat
+                     ;; :query-counter-bits
                      :max-server-wait-timeout
+                     ;; 23.82
                      :min-fragment-interpolation-offset
                      :max-fragment-interpolation-offset
                      :fragment-interpolation-offset-bits
+                     :max-draw-buffers
+                     :max-dual-source-draw-buffers
+                     :max-color-attachments
+                     ;; 23.84 implementation dependent transform
+                     ;; feedback limits
                      :max-transform-feedback-interleaved-components
                      :max-transform-feedback-separate-attribs
                      :max-transform-feedback-separate-components
                      :max-transform-feedback-buffers
-                     :max-dual-source-draw-buffers
+                     ;; ??
+                     :smooth-point-size-range
+                     :smooth-point-size-granularity
+                     :line-width-range
+                     :line-width-granularity
+                     :max-combined-image-units-and-fragment-outputs
                      :aux-buffers
-                     :max-draw-buffers
-                     :max-color-attachments
                      :implementation-color-read-format)
           do (format t "~s: ~a~%" i (get* i)))
+    (let ((slv (ignore-errors (gl:get* :num-shading-language-versions))))
+      (when slv
+        (format t "shading language versions:~%~{  ~s~%~}"
+                (loop for i below slv collect (gl:get-string-i :shading-language-version i)))))
+    (format t "max anisotropy: ~s~%" (gl:get-float :max-texture-max-anisotropy-ext 1))
     (when *gl-info-verbose*
       (format t "----~%")
       (loop for (i nil nil m) in gl::*query-enum-sizes*
