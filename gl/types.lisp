1146b624a2453114a8aa0e2d954911e4e3ab5c8e
Bart Botta <00003b@gmail.com>
2023-12-20 08:05:44 -0600

    use sized types to match gl.xml
    but keep unsized types where gl.xml uses them, even if pdf spec specifies
    a size

--- gl/types.lisp
+++ gl/types.lisp
@@ -37,7 +37,7 @@
 
 (define-foreign-type ensure-integer ()
   ()
-  (:actual-type :int)
+  (:actual-type :int32)
   (:simple-parser ensure-integer))
 
 (defmethod translate-to-foreign (value (type ensure-integer))
@@ -97,25 +97,47 @@
             ,value)))))
 
 ;;;; Deftypes
+;; spec lists exact sizes for most of these, but gl.xml defines them
+;; with C types, so using the C types here to match the headers
 
-(defctype boolean (:boolean :unsigned-char))
-(defctype bitfield :unsigned-int)
+;; to gl, 0->false, others-> true. from gl,false->0,true->1
+(defctype boolean (:boolean :unsigned-char)) ;; spec = 8 bit, sign not specified
+(defctype bitfield :unsigned-int) ;; spec = 32 bits, sign not specified
 
-(defctype char :char)
+(defctype byte :int8)
+(defctype ubyte :uint8)
+
+(defctype short :int16)
+(defctype ushort :uint16)
+
+(defctype int ensure-integer) ;; gl.xml = :int, spec = :int32
+(defctype uint :unsigned-integer) ;; spec = :uint32
+(defctype clampx :int32)
+(defctype sizei ensure-integer) ;; gl.xml = :int, spec = :uint32?
+
+(defctype float ensure-float)
+(defctype clampf ensure-float)
+(defctype double ensure-double)
+(defctype clampd ensure-double)
+
+(defctype char :char) ;; spec = 8 bit, sign not specified
 (defctype char-arb :char)
-(defctype handle-arb :unsigned-int)
-(defctype byte :char)
-(defctype short :short)
-(defctype int ensure-integer)
-(defctype sizei ensure-integer)
-(defctype ubyte :unsigned-char)
-(defctype ushort :unsigned-short)
-(defctype uint :unsigned-int)
-(defctype half :unsigned-short)
-(defctype half-arb :unsigned-short)
-(defctype half-nv :unsigned-short)
+
+(defctype handle-arb #+darwin :pointer #-darwin :uint32)
+
+(defctype half :uint16)
+(defctype half-arb :uint16)
+(defctype half-nv :uint16)
+(defctype fixed :int32)
+
+
+;; enum is 'unsigned int', spec = 32
+
+
 (defctype int64 :int64)
 (defctype uint64 :uint64)
+(defctype int64-ext :int64)
+(defctype uint64-ext :uint64)
 
 (defctype void :void)
 (defctype string :string)
@@ -124,29 +146,20 @@
   ;; try to figure out a good size for intptr/ptrdiff_t and sizeiptr
   (cond
     ((= (foreign-type-size :pointer)
-        (foreign-type-size :long))
-     (defctype ptrdiff-t :long)
-     (defctype sizeiptr :long))
+        (foreign-type-size :uint32))
+     (defctype ptrdiff-t :int32)
+     (defctype sizeiptr :uint32))
     ((= (foreign-type-size :pointer)
-        (foreign-type-size :long-long))
-     (defctype ptrdiff-t :long-long)
-     (defctype sizeiptr :long-long ))
+        (foreign-type-size :uint64))
+     (defctype ptrdiff-t :int64)
+     (defctype sizeiptr :uint64))
     (t (error "not sure how big ptrdiff-t is on this platform"))))
 
 (defctype intptr ptrdiff-t)
 (defctype intptr-arb ptrdiff-t)
 (defctype sizeiptr-arb sizeiptr)
 
-(defctype float ensure-float)
-(defctype clampf ensure-float)
-(defctype double ensure-double)
-(defctype clampd ensure-double)
-
-(defctype clampx :int)
 (defctype egl-image-oes (:pointer :void))
-(defctype fixed int)
-(defctype int64-ext :int64)
-(defctype uint64-ext :uint64)
 (defctype sync (:pointer)) ;; struct __GLsync *
 (defctype vdpau-surface-nv intptr)
 (defctype egl-client-buffer-ext (:pointer))
